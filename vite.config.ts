fda6441af77099910090705f2bf4e69bf59c802c
Chris Page <chris@hephae.co>
2025-12-29 12:25:27 -0500

    added defaults

--- vite.config.ts
+++ vite.config.ts
@@ -13,11 +13,11 @@ export default defineConfig(({ mode }) => {
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.AI_READINESS_QUEST_URL': JSON.stringify(env.AI_READINESS_QUEST_URL),
-        'process.env.TRAFFIC_FORECASTER_URL': JSON.stringify(env.TRAFFIC_FORECASTER_URL),
-        'process.env.AETHERIA_AI_RESTAURANT_URL': JSON.stringify(env.AETHERIA_AI_RESTAURANT_URL),
-        'process.env.AI_PRODUCT_MOCKUP_URL': JSON.stringify(env.AI_PRODUCT_MOCKUP_URL),
-        'process.env.MARKETING_BUSINESS_AI_URL': JSON.stringify(env.MARKETING_BUSINESS_AI_URL)
+        'process.env.AI_READINESS_QUEST_URL': JSON.stringify(env.AI_READINESS_QUEST_URL || "https://quest.hephae.co"),
+        'process.env.TRAFFIC_FORECASTER_URL': JSON.stringify(env.TRAFFIC_FORECASTER_URL || "https://traffic-prediction.hephae.co"),
+        'process.env.AETHERIA_AI_RESTAURANT_URL': JSON.stringify(env.AETHERIA_AI_RESTAURANT_URL || "https://aetheria.hephae.co"),
+        'process.env.AI_PRODUCT_MOCKUP_URL': JSON.stringify(env.AI_PRODUCT_MOCKUP_URL || "https://aistudio.google.com/apps/bundled/product_mockup?showPreview=true&showAssistant=true"),
+        'process.env.MARKETING_BUSINESS_AI_URL': JSON.stringify(env.MARKETING_BUSINESS_AI_URL || "https://chatgpt.com/g/g-Wud3tXQj3-marketing-business-ai")
       },
       resolve: {
         alias: {
